74ce84efcc281cc2ab88dac6268a3eb39d85d3ee
Mark John <markjohnjopia1@gmail.com>
2024-10-31 16:54:11 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Redirect root URL to /login
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/student-profile', function () {
+    return view('student_profile.student_profile');
+})->middleware(['auth', 'verified'])->name('student.profile');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

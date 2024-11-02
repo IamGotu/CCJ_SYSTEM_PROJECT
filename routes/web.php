16d2dce50639f6d7c0cc5d850742365feb5797a7
Mark John <markjohnjopia1@gmail.com>
2024-11-02 12:37:56 +0800

    student profile route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Redirect root URL to /login
@@ -8,9 +9,9 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::get('/student-profile', function () {
+Route::get('/dashboard', function () {
     return view('student_profile.student_profile');
-})->middleware(['auth', 'verified'])->name('student.profile');
+})->name('student.profile');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

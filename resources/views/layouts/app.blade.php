25cf5fe7200d95f7080e7bc1fa9dc50f24c7775d
Mark John <markjohnjopia1@gmail.com>
2024-11-06 20:52:32 +0800

    fixing bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
             <!-- Page Content -->
             <main>
-                @yield('content') <!-- This is where the content will go -->
+                {{ $slot }}
             </main>
         </div>
     </body>
